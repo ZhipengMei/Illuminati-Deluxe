ec96890781af3c2904a1d951c5403e518c878226
Adrian Mei <zmei10@mail.ccsf.edu>
2017-02-19 18:48:23 -0800

    hello world statement

--- Sample Project/src/testing.java
+++ Sample Project/src/testing.java
@@ -1,5 +1,8 @@
 
 public class testing {
-	//YOLO from PizzaPat1
-
+	
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+    
 }
