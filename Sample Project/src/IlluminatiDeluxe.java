f381d3c0da28e53d407d94e92a22aab54aec50b2
PizzaPat <patpongtana@gmail.com>
2017-02-20 20:56:44 -0800

    Login Page Drafting

--- Sample Project/src/IlluminatiDeluxe.java
+++ Sample Project/src/IlluminatiDeluxe.java
@@ -2,19 +2,19 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.Font;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
+import javax.swing.ImageIcon;
 
 public class IlluminatiDeluxe {
 
 	private JFrame frame;
-	private JTextField username;
-	private JTextField password;
+	private JTextField usernameField;
+	private JTextField passwordField;
 
 	/**
 	 * Launch the application.
@@ -48,70 +48,76 @@ public class IlluminatiDeluxe {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		
-		
 		JPanel LoginPage = new JPanel();
 		LoginPage.setBounds(0, 0, 1184, 681);
 		frame.getContentPane().add(LoginPage);
 		LoginPage.setLayout(null);
 		
+		JPanel SecondPage = new JPanel();
+		SecondPage.setBounds(0, 0, 1184, 681);
+		frame.getContentPane().add(SecondPage);
+		SecondPage.setLayout(null);
+		SecondPage.setVisible(false);
+		
 		JLabel lblUsername = new JLabel("Username");
-		lblUsername.setFont(new Font("Garamond", Font.PLAIN, 29));
-		lblUsername.setBounds(87, 151, 115, 41);
+		lblUsername.setFont(new Font("Lucida Fax", Font.BOLD, 32));
+		lblUsername.setBounds(81, 159, 169, 48);
 		LoginPage.add(lblUsername);
 		
 		JLabel lblPassword = new JLabel("Password");
-		lblPassword.setFont(new Font("Garamond", Font.PLAIN, 29));
-		lblPassword.setBounds(87, 217, 115, 41);
+		lblPassword.setFont(new Font("Lucida Fax", Font.BOLD, 32));
+		lblPassword.setBounds(81, 259, 169, 48);
 		LoginPage.add(lblPassword);
 		
-		username = new JTextField();
-		username.setFont(new Font("Tahoma", Font.PLAIN, 21));
-		username.setBounds(238, 151, 256, 41);
-		LoginPage.add(username);
-		username.setColumns(10);
+		usernameField = new JTextField();
+		usernameField.setFont(new Font("Tahoma", Font.PLAIN, 28));
+		usernameField.setBounds(260, 159, 244, 48);
+		LoginPage.add(usernameField);
+		usernameField.setColumns(10);
 		
-		password = new JTextField();
-		password.setFont(new Font("Tahoma", Font.PLAIN, 21));
-		password.setColumns(10);
-		password.setBounds(238, 217, 256, 41);
-		LoginPage.add(password);
+		passwordField = new JTextField();
+		passwordField.setFont(new Font("Tahoma", Font.PLAIN, 28));
+		passwordField.setColumns(10);
+		passwordField.setBounds(260, 260, 244, 48);
+		LoginPage.add(passwordField);
 		
-		JButton btnLogin = new JButton("Login");
-		btnLogin.addActionListener(new ActionListener() {
+		JButton btnNewButton = new JButton("Login");
+		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String uname = username.getText();
-				String pass = password.getText();
+				String uname = usernameField.getText();
+				String pass = passwordField.getText();
 				
 				if(uname.equals("admin") && pass.equals("password")){
 					LoginPage.setVisible(false);
-					
+					SecondPage.setVisible(true);
 				}
 			}
 		});
-		btnLogin.setFont(new Font("Arial Black", Font.PLAIN, 24));
-		btnLogin.setBounds(540, 151, 136, 93);
-		LoginPage.add(btnLogin);
+		btnNewButton.setFont(new Font("Lucida Fax", Font.ITALIC, 24));
+		btnNewButton.setBounds(574, 259, 203, 48);
+		LoginPage.add(btnNewButton);
 		
-		JPanel Success = new JPanel();
-		Success.setBounds(0, 0, 1184, 681);
-		frame.getContentPane().add(Success);
-		Success.setLayout(null);
+		JLabel LoginPageBackground = new JLabel("");
+		LoginPageBackground.setIcon(new ImageIcon("C:\\Users\\YOLO\\Desktop\\CSULB Spring2017\\IlluminatiDeluxe\\Sample Project\\graphics\\illuminatiBackground.jpg"));
+		LoginPageBackground.setBounds(-300, 0, 1484, 681);
+		LoginPage.add(LoginPageBackground);
 		
-		JLabel lblSuckItYolo = new JLabel("Suck it, YOLO");
-		lblSuckItYolo.setFont(new Font("Tahoma", Font.PLAIN, 28));
-		lblSuckItYolo.setBounds(698, 164, 170, 89);
-		Success.add(lblSuckItYolo);
-		
-		JButton btnGoBack = new JButton("Go Back");
-		btnGoBack.addActionListener(new ActionListener() {
+		JButton btnNewButton_1 = new JButton("Illuminati Confirmed");
+		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				LoginPage.setVisible(true);
-				Success.setVisible(false);
+				SecondPage.setVisible(false);
 			}
 		});
-		btnGoBack.setFont(new Font("Tahoma", Font.PLAIN, 22));
-		btnGoBack.setBounds(721, 309, 170, 70);
-		Success.add(btnGoBack);
+		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnNewButton_1.setBounds(77, 180, 251, 80);
+		SecondPage.add(btnNewButton_1);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\YOLO\\Desktop\\CSULB Spring2017\\IlluminatiDeluxe\\Sample Project\\graphics\\108_Blinker.gif"));
+		lblNewLabel.setBounds(199, 23, 1221, 712);
+		SecondPage.add(lblNewLabel);
+		
+		
 	}
 }
