a273d1f4940392c52627cd69a8accf0bea3642cd
AnimeMei <adrian.z.mei@gmail.com>
2017-04-18 21:16:42 -0700

    update dice rolling flow

--- IlluminatiDeluxe_Prototype/src/fxml/gameTableController.java
+++ IlluminatiDeluxe_Prototype/src/fxml/gameTableController.java
@@ -152,8 +152,13 @@ public class gameTableController extends Message implements Initializable  {
 	
 	// create an array list
     ArrayList players = new ArrayList();
-    ArrayList images = new ArrayList();
+    ArrayList profileImages = new ArrayList();
     ArrayList names = new ArrayList();
+    ArrayList diceImages = new ArrayList();
+    
+    //corner menu 
+    CornerMenu cornerMenu;
+
 
 
 
@@ -166,10 +171,13 @@ public class gameTableController extends Message implements Initializable  {
 		//display players profile
 		getPlayersData();
 		//store player imageview into arraylist
-		images.add(player1Image);
-		images.add(player2Image);
+		profileImages.add(player1Image);
+		profileImages.add(player2Image);
 		names.add(player1Name);
 		names.add(player2Name);
+		diceImages.add(diceImageview1);
+		diceImages.add(diceImageview2);
+		diceImages.add(diceImageview3);
 		
 		//rotate imageview
         RotateTransition animation = Animattion.createAnimation(diceImageview);
@@ -210,6 +218,12 @@ public class gameTableController extends Message implements Initializable  {
     	}    	
     }
     
+    @FXML
+    void mouseLeaveMenu(MouseEvent event) {
+        if(cornerMenu != null){
+    		cornerMenu.hide();	
+         }
+    }
    
     
     public void actionMenu(){
@@ -243,8 +257,9 @@ public class gameTableController extends Message implements Initializable  {
 
  		// corner menu
  		// position a corner menu in the top left corner, that initially is not visible
- 		CornerMenu cornerMenu = new CornerMenu(CornerMenu.Location.BOTTOM_LEFT, stackPane, true)
- 		    .withAnimationInterpolation(null)
+// 		CornerMenu cornerMenu = new CornerMenu(CornerMenu.Location.BOTTOM_LEFT, stackPane, true)
+ 		cornerMenu = new CornerMenu(CornerMenu.Location.BOTTOM_LEFT, stackPane, true)
+            .withAnimationInterpolation(null)
  		    .withAutoShowAndHide(true);
  		 
  		// add the menu items
@@ -271,14 +286,21 @@ public class gameTableController extends Message implements Initializable  {
 			    	players.clear();
 			    	for (DataSnapshot playerSnapshot:dataSnapshot.getChildren()){	
 			    		if(playerSnapshot.child("id").getValue().equals(currentUser.getUID())){
-			    			//do nothing
+			    			//display current user's dice
+			    		    if (playerSnapshot.hasChild("dice")) {
+			    		    	parseImageView((String) playerSnapshot.child("dice").getValue(), (ImageView)diceImageview3);
+			    		      }
 			    		} else {		
 			    			ArrayList player = new ArrayList();
 			    			player.add((String) playerSnapshot.getKey());
 			    			player.add((String) playerSnapshot.child("imageName").getValue());
+			    			//check if the "dice" child exist
+			    		    if (playerSnapshot.hasChild("dice")) {
+				    			player.add((String) playerSnapshot.child("dice").getValue());
+				    			System.out.println("dice name is:" + player.get(1));
+			    		    }
 			    			players.add(player);
-			    		}//end else
-			    		
+			    		}//end else			    		
 			    	}//end for
 			    	
 	    			Platform.runLater(new Runnable() {
@@ -287,18 +309,26 @@ public class gameTableController extends Message implements Initializable  {
 					    	for(int i=0; i<players.size(); i++){
 					    		ArrayList player = (ArrayList) players.get(i);			    		
 					    		
-			    				((Label) names.get(i)).setText((String)player.get(0));
+			    				((Label) names.get(i)).setText((String)player.get(0)); //display username into a label
+			    				
+			    				//parsing profileimage name into path
 			    				String path = new File("support/images/"+(String) player.get(1)).getAbsolutePath();
 			    				Image image = new Image(new File(path).toURI().toString());
-			    				((ImageView) images.get(i)).setImage(image);
+			    				((ImageView) profileImages.get(i)).setImage(image);
+//			    		    	parseImageView((String) player.get(1), (ImageView)profileImages.get(i));
+			    				
 					    	}//end for
-					    	//removeing legacy data when player size shrinks
+					    	//removing legacy data when player size shrinks
 					    	if(players.size() == 1) {
 			    				((Label) names.get(1)).setText(" ");
-			    				((ImageView) images.get(1)).setImage(null);
+			    				((ImageView) diceImages.get(1)).setImage(null);
 					    	}
 						}	
 					});
+	    			
+	    		    //this block will listen to any changes and display dice roll throughout the entire game
+	    			
+
 			    }
 			    @Override
 			    public void onCancelled(DatabaseError databaseError) {
@@ -307,21 +337,75 @@ public class gameTableController extends Message implements Initializable  {
 			});
     }
     
-//    //rolling dice 
-//    public void rollDice(){
-//    	
-//    }
 
     @FXML
     void rollDiceAction(MouseEvent event) {
+    	
     	if(diceRolled == false) {
+    		//generate 2 random numbers between 1-6
+        	final int[] dice = new Random().ints(1, 7).distinct().limit(2).toArray();
+        	    		
 			System.out.println("Rolling dice");
-			String path = new File("dice.png").getAbsolutePath();
-			Image image = new Image(new File(path).toURI().toString());
-			diceImageview3.setImage(image);
-			diceRolled = true;
-		}
+			String diceImageName = String.format("%s-%s.gif",dice[0],dice[1]);
+//			String path = String.format("support/images/dice/%s",diceImageName);
+//			String absolutePath = new File(path).getAbsolutePath();
+//			Image image = new Image(new File(absolutePath).toURI().toString());
+//			diceImageview3.setImage(image); //display the image
+			
+			diceRolled = true; //play only roll dice once each turn
+		
+			//upload the dice roll to firebase
+    		final DatabaseReference ref = rootRef.child(currentUser.getCurrentChannel()).child(currentUser.getName()).child("dice");
+    		ref.setValue(diceImageName, new DatabaseReference.CompletionListener() {
+    		    @Override
+    		    public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+    		        if (databaseError != null) {
+    		            System.out.println("Dice date could not be saved " + databaseError.getMessage());
+    		        } else {
+    		            System.out.println("Dice date saved successfully.");
+    		        }
+    		    }
+    		}); //end upload dice data to database
+    		
+		}//end if
     }
+    
+//    this method will listen to any changes and display dice roll throughout the entire game
+//    public void diceRollListener(){
+//    	//database reference to specific "chat > channel name" node
+//		  final DatabaseReference d = rootRef.child(currentUser.getCurrentChannel());
+//		  
+//		  // Attach a listener to read the data at our posts reference
+//		  playerRef.addValueEventListener(new ValueEventListener() {
+//	
+//			    @Override
+//			    public void onDataChange(DataSnapshot dataSnapshot) {
+//			    	players.clear();
+//			    	for (DataSnapshot playerSnapshot:dataSnapshot.getChildren()){	
+////			    		if(playerSnapshot.child("id").getValue().equals(currentUser.getUID())){
+////			    			//do nothing
+////			    		} else {		
+////			    			ArrayList player = new ArrayList();
+////			    			player.add((String) playerSnapshot.getKey());
+////			    			player.add((String) playerSnapshot.child("imageName").getValue());
+////			    			players.add(player);
+////			    		}//end else
+//			    		
+//			    	}//end for
+//			    	
+//	    			Platform.runLater(new Runnable() {
+//						@Override
+//						public void run() {
+//					
+//						}	
+//					});
+//			    }
+//			    @Override
+//			    public void onCancelled(DatabaseError databaseError) {
+//
+//			    }
+//			});//end listener
+//    }
     
     public void resizePowerStructureGrid(){
 //    	powerStructureGrid
@@ -338,6 +422,16 @@ public class gameTableController extends Message implements Initializable  {
 
     	
 
+    }
+    
+    
+    //this method will parse imageView with a image name
+    public void parseImageView(String imageName, ImageView imageview) {
+		//parsing dice image name into path	
+		String path = String.format("support/images/dice/%s",imageName);
+		String absolutePath = new File(path).getAbsolutePath();
+		Image image = new Image(new File(absolutePath).toURI().toString());
+		imageview.setImage(image); //display the image
     }
    
 
